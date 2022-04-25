d0eb5274f86262577effc45a034a3f0b66043d58
Louis E Mauget <mauget@users.noreply.github.com>
2022-04-25 08:00:17 -0400

    Update profile_icon.js
    Formatting

--- cypress/integration/profile_icon/profile_icon.js
+++ cypress/integration/profile_icon/profile_icon.js
@@ -4,6 +4,7 @@ Given('I open the app', () => {
     cy
         .visit('/')
 });
+
 And('I click sign-in', () => {
     // Sign in Bob Smith by pressing the first button
     cy.get('button')
@@ -12,21 +13,25 @@ And('I click sign-in', () => {
         .get('[data-qa="App.StyledProfileIcon"]')
         .should('be.visible')
 });
+
 And('I click the profile icon', () => {
     cy.get('[data-qa="App.StyledProfileIcon"]')
         .click()
         .get('[data-qa="App.StyledProfileIcon.ProfileMenu.StyledRelativeContainer.StyledMenu.StyledButton"]')
         .should('be.visible')
 });
+
 Then('it displays the user profile',()=>{
         cy
             .get('[data-qa="App.StyledProfileIcon.ProfileMenu.StyledRelativeContainer.StyledMenu.StyledButton"]')
             .should('be.visible')
 });
+
 When('I click the sign-out button', () => {
     cy.get('[data-qa="App.StyledProfileIcon.ProfileMenu.StyledRelativeContainer.StyledMenu.StyledButton"]')
         .click();
 });
+
 Then('"signed out" should be visible', () => {
     cy.get('[data-qa="App.signed-out"]')
         .should('be.visible');
